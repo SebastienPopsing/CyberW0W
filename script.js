2472508f494d78b3fef6c8ebdfefacce9c5fb508
SebastienPopsing <165157737+SebastienPopsing@users.noreply.github.com>
2024-05-01 17:10:26 +0200

    js nav + article principal
    commencement de la creation des boucle pour mettre chaque article la ou il faut

--- script.js
+++ script.js
@@ -11,29 +11,59 @@ function getData() {
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
 
+
 //NOM JOURNAL
 
-       let nomJournal = data.journal.nomJournal;
-       console.log(nomJournal);
+let nomJournal = data.journal.nomJournal;
+console.log(nomJournal);
+let nomJournalReplacment = document.querySelector('.mag_name');
 
-       let nomJournalReplacment = document.querySelector('.mag_name');
+nomJournalReplacment.textContent = nomJournal;
 
-       nomJournalReplacment.textContent = nomJournal;
-       
-      //APPEL ACTION
+//APPEL ACTION
 
-       let texteAppelAction = data.journal.texteAppelAction;
-       console.log(texteAppelAction);
+let texteAppelAction = data.journal.texteAppelAction;
+console.log(texteAppelAction);
+let texteAppelActionReplacment = document.querySelector('#appel_action');
 
-       let texteAppelActionReplacment = document.querySelector('#appel_action');
+texteAppelActionReplacment.textContent = texteAppelAction;
 
-       texteAppelActionReplacment.textContent = texteAppelAction;
+//Article principal
 
-       /// FIN DU CODE
-     })
-     .catch((error) => console.error('Erreur lors de la lecture des données :', error));
+let articlePrincipalTitle = data.journal.articlePrincipal.titre;
+console.log(articlePrincipalTitle);
+let articlePrincipalTitleReplacment = document.querySelector('#article_principal_title');
+articlePrincipalTitleReplacment.textContent = articlePrincipalTitle;
+
+let articlePrincipalTheme = data.journal.articlePrincipal.theme;
+console.log(articlePrincipalTheme);
+let articlePrincipalDate = data.journal.articlePrincipal.date;
+console.log(articlePrincipalDate);
+let articlePrincipalThemeDateReplacment = document.querySelector('#article_principal_theme_date');
+articlePrincipalThemeDateReplacment.textContent = `${articlePrincipalTheme} - ${articlePrincipalDate}`;
+
+let articlePrincipalDescription = data.journal.articlePrincipal.description;
+console.log(articlePrincipalDescription);
+let articlePrincipalDescriptionReplacment = document.querySelector('#article_principal_description');
+articlePrincipalDescriptionReplacment.textContent = articlePrincipalDescription;
+
+
+let articleTitre = data.journal.articles[0].titre;
+console.log(articleTitre);
+let articleTheme = data.journal.articles[0].theme;
+console.log(articleTheme);
+let articleDate = data.journal.articles[0].date;
+console.log(articleDate);
+let articleImage = data.journal.articles[0].image;
+console.log(articleImage);
+
+
+/// FIN DU CODE
+})
+ .catch((error) => console.error('Erreur lors de la lecture des données :', error));
  }
- 
+ ///ON écrit les fonctions ici
+
  getData();
 
  ///ON écrit les fonctions ici
